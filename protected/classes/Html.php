33cd7c2c28a25a962962e8c251049d9a5ebf5fe0
Vasin Semen <vsp.tima@gmail.com>
2017-09-07 19:22:48 +0300

    Заготовка класса статической библиотеки

---
+++ protected/classes/Html.php
@@ -0,0 +1,19 @@
+<?php
+class Html {
+
+  public static function link()
+  {
+    //
+  }
+  
+  public static function menuLink()
+  {
+    //
+  } 
+  
+  public static function printArrat()
+  {
+    //
+  }  
+
+}
