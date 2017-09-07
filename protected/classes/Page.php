68d7faef154f50c6da8c88dc8f421f4b60622ed2
Vasin Semen <vsp.tima@gmail.com>
2017-09-07 19:20:44 +0300

    Создана заготовка класса Page расширенного от Seo

---
+++ protected/classes/Page.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'Seop.php';
+class Page extends Seo {
+
+  public function header()
+  {
+    //
+  }
+  
+  public function footer()
+  {
+    //
+  } 
+  
+  public function loadData()
+  {
+    //
+  }  
+  
+}
