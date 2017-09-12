0b9761d06b739e7a63a756c5b574a16925b05182
Vasin Semen <vsp.tima@gmail.com>
2017-09-12 19:59:40 +0300

    Доработка Page.

--- protected/classes/Page.php
+++ protected/classes/Page.php
@@ -1,20 +1,29 @@
 <?php
-require_once 'Seop.php';
+require_once 'Seo.php';
 class Page extends Seo {
 
-  public function header()
-  {
-    //
-  }
-  
-  public function footer()
-  {
-    //
-  } 
-  
-  public function loadData()
-  {
-    //
-  }  
-  
+    /**
+     * Загрузка верха страницы.
+     */
+    public function header()
+    {
+        //
+    }
+
+    /**
+     * Загрузка низа страницы.
+     */
+    public function footer()
+    {
+        //
+    }
+
+    /**
+     * Загрузка данных.
+     */
+    public function loadData()
+    {
+        //
+    }
+
 }
