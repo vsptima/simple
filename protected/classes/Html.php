a858d0fd2fe9e56a3ecfb30240ae4e18ca8bfa88
Vasin Semen <vsp.tima@gmail.com>
2017-09-12 20:22:16 +0300

    Доработка класса Html.

--- protected/classes/Html.php
+++ protected/classes/Html.php
@@ -1,19 +1,37 @@
 <?php
 class Html {
 
-  public static function link()
-  {
-    //
-  }
-  
-  public static function menuLink()
-  {
-    //
-  } 
-  
-  public static function printArrat()
-  {
-    //
-  }  
+    public static function link()
+    {
+        //
+    }
+
+    /**
+     * Формирование пункта меню.
+     * @param $href
+     * @param $text
+     * @return string
+     */
+    public static function menuLink($href,$text)
+    {
+        $path = $_SERVER['REQUEST_URI'];
+        $active = '';
+        if ($href == $path) {
+            $active = 'class="active"';
+        }
+        $str = "<li {$active} role=\"presentation\"><a href=\"{$href}\">{$text}</a></li>";
+        return $str;
+    }
+
+    /**
+     * Распечатка массива в браузере.
+     * @param $arr
+     */
+    public static function printArray($arr)
+    {
+        echo '<pre>';
+        print_r($arr);
+        echo '</pre>';
+    }
 
 }
