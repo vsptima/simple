ca9f83efe79755af7718edb096bbbac328d2d55e
Vasin Semen <vsp.tima@gmail.com>
2017-09-12 21:05:02 +0300

    Файл загрузки классов и создания их объектов

---
+++ protected/views/layouts/_classes.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: papasha
+ * Date: 12.09.2017
+ * Time: 20:45
+ */
+require_once 'protected/classes/Page.php';
+require_once 'protected/classes/Html.php';
+$page = new Page();
