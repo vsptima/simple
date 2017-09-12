a0320334eb324f942fe74db9bfa35b3fe176bdff
Vasin Semen <vsp.tima@gmail.com>
2017-09-12 20:22:40 +0300

    Формирование меню с помощью класса Html.

--- protected/views/layouts/_menu.php
+++ protected/views/layouts/_menu.php
@@ -1,8 +1,9 @@
+<?php require_once $_SERVER['DOCUMENT_ROOT'].'/protected/classes/Html.php'; ?>
 <nav>
     <ul class="nav nav-pills pull-right">
-        <li role="presentation"><a href="/">Home</a></li>
-        <li role="presentation"><a href="/skill.php">Skill</a></li>
-        <li role="presentation"><a href="/jargon.php">Jargon</a></li>
-        <li role="presentation"><a href="/link.php">Link</a></li>
+        <?php echo Html::menuLink('/','Home')?>
+        <?php echo Html::menuLink('/skill.php','Skill')?>
+        <?php echo Html::menuLink('/jargon.php','Jargon')?>
+        <?php echo Html::menuLink('/link.php','Link')?>
     </ul>
 </nav>
