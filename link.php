1f87513b6e6fb8bcc10a77dbba13c354663d02fd
Vasin Semen <vsp.tima@gmail.com>
2017-09-14 19:59:00 +0300

    Вывод данных из файла json

--- link.php
+++ link.php
@@ -3,6 +3,8 @@ require_once 'protected/views/layouts/_classes.php';
 $page->title = 'Ссылки';
 $page->description = 'Адреса сайтов с полезной профессиональной информацией';
 $page->header();
+$json = file_get_contents('protected/data/link.json');
+$arr = json_decode($json);
 ?>
 
     <div class="jumbotron">
@@ -12,27 +14,9 @@ $page->header();
     </div>
 
     <div class="row marketing">
-        <div class="col-lg-6">
-            <h4>Subheading</h4>
-            <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-            <h4>Subheading</h4>
-            <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-            <h4>Subheading</h4>
-            <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-        </div>
-
-        <div class="col-lg-6">
-            <h4>Subheading</h4>
-            <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-            <h4>Subheading</h4>
-            <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-            <h4>Subheading</h4>
-            <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-        </div>
+        <?php foreach ($arr as $val) : ?>
+            <p><a href="<?= $val; ?>"><?= $val; ?></a></p>
+        <?php endforeach; ?>
     </div>
 
 <?php $page->footer() ?>
